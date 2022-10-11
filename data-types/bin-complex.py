e947e9839b211ee79e7540d54a5d3d01aa105234
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-10-11 17:23:09 +0800

    add bin and complex info

---
+++ data-types/bin-complex.py
@@ -0,0 +1,9 @@
+# complex - extra data type
+
+# int float
+
+#bin5 - returns the binary of the number - 0b = telling us it is a binary number
+
+# extra resource - https://www.geeksforgeeks.org/python-int-function/
+
+# int('0b101', 2) = 5
---
+++ data-types/operator-precedence.py
@@ -0,0 +1,32 @@
+# dif operators have dif precedence over dif numbers
+
+# () 
+# **
+# * /
+# + -
+
+#BIDMAS
+
+
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
+
+print((5 + 4) * 10 / 2)
+
+# 9 * 5 = 45
+
+print(((5 + 4) * 10) / 2)
+
+# 45
+
+print((5 + 4) * (10 / 2))
+
+#45
+
+print(5 + (4 * 10) / 2)
+
+# 25
+
+print(5 + 4 * 10 // 2)
+
+# 25
