582c2bf367a2e9dbd39a05b4aa402a5edb1d17cf
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-22 14:29:23 +0800

    list-slicing

---
+++ data-types/list-slicing.py
@@ -0,0 +1,20 @@
+# List slicing 
+
+amazon_cart = [
+    'book', 
+    'treadmill',
+    'toys',
+    'grapes',
+    ]
+
+amazon_cart[0] = 'laptop'
+# will replace book with laptop
+
+# lists are mutable unlike strings
+# in list slicing you create a new list - you can assign to a new variable
+
+# need to look up more - copying versus modifying lists
+
+# to copy a list do this:
+# new_cart = amazon_cart[:] (this will copy rather than updating the original list)
+
---
+++ data-types/list.py
@@ -0,0 +1,16 @@
+# A list is a Data Structure
+
+li = [1,2,3,4]
+li2 = ['b','c',7, True]
+
+# lists are extremely important (in ppython - lists are a form of arrays)
+# they are a collection of items
+# This is a data structure
+# a way to organise informatoin into say like a folder
+# eg food stores food
+# or eg backpack - stuff everything in there but hard to find things
+# a container around your data that has different pros and cons
+# [] allow us to contain information
+
+amazon_cart =['book', 'treadmill']
+print(amazon_cart[0])
