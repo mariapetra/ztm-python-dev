5cc3b588b981f0459eb6784e63ba42d0e4e29c26
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-22 09:11:33 +0800

    exercise-type-conversion

---
+++ variables/booleans.py
@@ -0,0 +1,8 @@
+#booleans
+
+name = 'Maria'
+is_cool = False
+is_cool = True
+
+bool(1) = True
+bool(0) = False
--- variables/type-conversion.py
+++ variables/type-conversion.py
@@ -3,3 +3,10 @@
 print(type(int(str(100))))
 
 # turns 200 into string and then number
+
+name = 'Maria Claydon'
+relationship_status = 'married'
+current_year = 2023
+birth_year = (input('what year were you born?'))
+age = current_year - int(birth_year)
+print(f"your age is {age}")
