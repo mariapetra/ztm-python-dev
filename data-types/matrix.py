55bf3fce4c077a0c0074069d29a9fe84d5edb29b
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-22 15:43:27 +0800

    matrix

---
+++ data-types/matrix.py
@@ -0,0 +1,25 @@
+# Matrix 
+# A way to describe two/multi dimensional lists
+
+[[]] an array with another array inside of it
+
+matrix = [
+    [[1],2,3]
+    [2,4,6]
+    [7,8,9]
+]
+
+# why?
+# This comes up alot on machine learning
+# eg a computer doesnt know what a photo of a dog is
+# a computer can understand an image based on pixels on a screen
+
+eg pixels could be:
+
+matrix2 = [
+    [1,0,1]
+    [0,1,0]
+    [1,1,0]
+]
+# matrix[0] = first array
+# matrix[0][1] = 0 takes the [first array] / [second item]
