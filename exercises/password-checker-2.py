100b91fa5e446db189faae9ac06ff5d6dfb2c146
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-22 14:14:47 +0800

    password-checker-challenge

---
+++ exercises/password-checker-2.py
@@ -0,0 +1,11 @@
+# Create 2 inputs - username and password
+# Print the password secret and password length
+# convert password to ******* (print * x password(len))
+# Should have a greeting
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+pw_length = int(len(password))
+scrambled_pw = ('*' * pw_length)
+
+print(f"Hi {username.capitalize()}, your password, {scrambled_pw} is {pw_length} characters long")
