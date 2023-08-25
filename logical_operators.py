d91392257bed7d58fe357f0dddbaac806146dc41
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-25 11:29:08 +0800

    logical-operator practice

--- logical_operators.py
+++ logical_operators.py
@@ -13,3 +13,14 @@ not
 # not means the opposite eg not True = false
 # not is a keyword and also a function: print(not(True))
 
+is_magician = False
+is_expert = True
+
+#check if magician AND expert - if True print you are a master magician
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician and not is_expert:
+    print('at least youre getting there')
+else:
+    print('you need magic powers')
