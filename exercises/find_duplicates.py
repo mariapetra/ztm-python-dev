2d57f9d418bd76c69d11a69223538c26e038699c
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-08-30 18:16:14 +0800

    find-duplicates exercise

---
+++ exercises/find_duplicates.py
@@ -0,0 +1,21 @@
+some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
+
+# find-duplicate
+duplicates = []
+
+for letter in some_list:
+    if some_list.count(letter) > 1:
+        duplicates.append(letter)
+
+print(duplicates)
+
+
+another_list = ["z", "ff", "f", "z", "z"]
+
+morethan1 = []
+
+for char in another_list:
+    if another_list.count(char) > 2:
+        morethan1.append(char)
+
+print(morethan1)
