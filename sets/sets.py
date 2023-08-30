ac0d346bf30cb6b57d45b7a67f76f94b65d94398
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-08-30 18:16:30 +0800

    comment commented code

--- sets/sets.py
+++ sets/sets.py
@@ -1,12 +1,12 @@
 # data structure
 # unordered collections of unique objects
-my_set = {1,2,3,4,5,5}
+my_set = {1, 2, 3, 4, 5, 5}
 # would only return 1 of the 5's
 my_set.add(100)
 
-how would we change a list to a set?
+# how would we change a list to a set?
 
-my_list = [1,2,3,4,5,6]
+my_list = [1, 2, 3, 4, 5, 6]
 set(my_list)
 # will return the list as a set removing all duplicates
 
