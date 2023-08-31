da0852c4dab7f9f40562f6c1c652f618dcf99952
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-31 09:15:46 +0800

    functions

---
+++ functions/functions.py
@@ -0,0 +1,8 @@
+ 
+# def = define. the () let the interpreter know you want to run say_hello. Good to keep code DRY
+
+def say_hello():
+    print('helloooooo')
+
+say_hello()
+
---
+++ functions/return.py
@@ -0,0 +1,6 @@
+def sum(num1, num2):
+    return num1 + num2
+
+print(sum(4,5))
+
+# a function either modifies something in our program or returns something
