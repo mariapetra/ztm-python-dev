0620864733e3e037d421ec409f9cdc000c54479e
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-31 14:32:50 +0800

    doctrsings

---
+++ python_basics/method_vs_functions.py
@@ -0,0 +1,20 @@
+methods vs functions
+
+list()
+print()
+max()
+max()
+min()
+input()
+
+def some_func():
+    pass
+
+some_func()
+
+# method = . eg. .capitalize() - a method has to be owned by something
+# what owns the method? whatever is left of the . built in objects that have methods, owned by an object or a data type
+# you can build your own methods
+
+# both allow us to take actions on our data types - they allow our programs to do something
+# the dif is minimal but it is the .
