9431bd4a53c434eca37aba2d559d38c2b31e9aec
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-08-31 13:42:47 +0800

    functions

--- functions/return.py
+++ functions/return.py
@@ -4,3 +4,20 @@ def sum(num1, num2):
 print(sum(4,5))
 
 # a function either modifies something in our program or returns something
+
+# good practice:
+
+# a function should do one thing really well
+# and usually a function should return something
+
+# you can also nest functions but this would not be easy to read and shouldnt be used
+
+def sum(num1, num2):
+    def func_2(num1, num2):
+        return num1 + num2
+    return func_2(num1, num2)
+
+total = sum(10,20)
+print(total)
+
+# prints 30
