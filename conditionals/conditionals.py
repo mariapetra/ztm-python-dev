9dd3008bb738ad8b16f41622943ad6b2f45d19a3
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-02 12:26:20 +0800

    update code per pylint suggestion

--- conditionals/conditionals.py
+++ conditionals/conditionals.py
@@ -1,10 +1,14 @@
-# in python we can use 'if' to see if some condition exists
-is_old = True
-is_licenced = True
+""""
+in python we can use 'if' to see if some condition exists
 
-if is_old and is_licensed: 
-    print('you are old enough and licensed to drive')
-elif is_licensed:
-    print('you are licensed but not old enough')
+"""
+
+IS_OLD = True
+IS_LICENSED = True
+
+if IS_OLD and IS_LICENSED:
+    print("you are old enough and licensed to drive")
+elif IS_LICENSED:
+    print("you are licensed but not old enough")
 else:
-    print('you cant drive')
+    print("you cant drive")
