0973b42d0ef1555b8d33f13664dd32feeb51ffc2
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-09-04 08:28:58 +0800

    attributes and methods

---
+++ oop/attributes_and_methods.py
@@ -0,0 +1,14 @@
+#objects have attributes
+# OOP = repeatable, well organised and energy efficient
+# you must know and understand this
+# functional over procedural
+# mimics something from the real world
+# there are many magic methods with we will wee later
+
+help(list)
+help(player1)
+
+# will give you a blueprint
+#attributes will be unique to an object
+# class object attribute
+# eg a player has to have a membership
--- oop/creating_objects.py
+++ oop/creating_objects.py
@@ -15,14 +15,19 @@
 
 
 class PlayerCharacter:
+    #class object attribute
+    # not dynamic actual attribute on the class
+    # this will be true for all objects
+    # we can use anywhere
+    membership = True
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        if (PlayerCharacter.membership):
+            self.name = name
+            self.age = age
 
 
-    def run(self):
-        print("run")
-        return 'done'
+    def shout(self):
+        print(f'my name is {self.name}')
 
 
 player1 = PlayerCharacter('Maria', 38)
