4a03f84de481861601cd041b0ae33765923c5ca3
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-09-06 16:08:56 +0800

    multiple inheritance

---
+++ oop/multiple_inheritance.py
@@ -0,0 +1,38 @@
+class User():
+
+    def sign_in(self):
+        self.name = name
+        self.power = power
+
+class Wizard(User):
+    def __init__(self, name, power):
+        super()__init__(self, email)
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        # User.attack(self)
+        print(f'attacking with power of {self.power}) 
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f'attacking with arrows: arrows left- {self.num_arrows}) 
+
+    def run(self):
+        print('ran really fast')
+
+class HybridBorg(Wizard, Archer)
+    def __init__(self, name, power, arrows):
+        Archer.__init__(self, name, arrows)
+        Wizard.__init__(self, name, arrows)
+
+
+hb1 = HybridBorg('borgi', 50, 100)
+print(hb1.run())
+print(hb1.attack())
+
+
