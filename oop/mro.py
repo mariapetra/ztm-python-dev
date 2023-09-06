baa709046a520a2a080244d2709d99cf8b915d34
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-09-06 16:16:30 +0800

    mro

--- oop/mro.py
+++ oop/mro.py
@@ -1,9 +1,11 @@
 #MRO - method resolution order - a rule that python follows to dtermine when you run a method which one should you run
 # What is first in line?
+# you probably will never use this or be tested on it
 
 # Check this by doing:
 # https://replit.com/@aneagoie/MRO
 # http://www.srikanthtechnologies.com/blog/python/mro.aspx
+
 print(D.mro())
 
 class A:
