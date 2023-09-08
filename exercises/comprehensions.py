505c2b8d6184af5272674429db5650e18a6d87b6
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-09-08 13:57:00 +0800

    comprehension exercise

---
+++ exercises/comprehensions.py
@@ -0,0 +1,11 @@
+some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
+
+# find-duplicate
+duplicates = []
+
+duplicates = set{[x for x in some_list if some_list.count(x) > 1]}
+
+print(duplicates)
+
+
+
