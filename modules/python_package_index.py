4ef393b0f9f8643a04684f7903dbf84c28bd0474
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-15 18:35:26 +0800

    pypi

---
+++ modules/python_package_index.py
@@ -0,0 +1,10 @@
+# we can create our own modules which makes python really special
+# eg numpy
+# we can use each others code using pip install
+
+# https://pypi.org
+# you dont need to reinvent the wheel there are many packages
+# first check if something exists in standard library then go to pypi
+# beware not all projects are good
+# check the version compatible / release history / etc
+# you can also create your own packages
