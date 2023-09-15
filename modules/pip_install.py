a1a2813dabd0964a02729e87e475cdfaf72f985e
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-15 20:04:35 +0800

    what are virtual envs and how do we use pip

--- modules/pip_install.py
+++ modules/pip_install.py
@@ -4,5 +4,20 @@ joke = pyjokes.get_joke('en', 'neutral')
 print(joke)
 
 # you can import via pycharm in project > external libraries
-# this is avialable only in your project
+# this is available only in your project
 # or you can install via pip
+
+# libraries are maintained by programmers - there will always be mistakes and bigs you can improve on
+# versioning = the first number = update / second = new release / last = breaking changes or major versions
+
+# pip3 list - show everything you have installed
+
+# uninstall:
+# pip3 uninstall pyjokes
+# or version:
+
+# pipenv - allows you to use pip and also a virtual env to install packages based on each package
+
+# venv is a virtual environment created by python - it is like a separate universe
+# you should create projects in virtual environments
+
