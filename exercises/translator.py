f85deb9c6764e31970d75ce399601381dc014873
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-18 18:26:36 +0800

    translator exercise

--- exercises/translator.py
+++ exercises/translator.py
@@ -2,7 +2,7 @@ from translate import Translator
 translator = Translator(to_lang="ja")
 
 try:
-    with open('./translator.txt', mode='r+') as translated:
+    with open('./translator.txt', mode='r') as translated:
         text = translated.read()
         translation = translator.translate(text)
         print(translation)
