582ec82f9bb9904be04b22e777dd9486bac2cbef
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-09-19 15:07:50 +0800

    improve unit test

--- testing/test.py
+++ testing/test.py
@@ -2,6 +2,9 @@ import unittest
 import testing
 
 class TestTesting(unittest.TestCase)
+    def setUp(self):
+        print('about to test a function')
+
     def test_do_stuff(self):
         test_param = 10
         result = testing.do_stuff(test_param)
@@ -11,7 +14,27 @@ class TestTesting(unittest.TestCase)
     def test_do_stuff2(self):
         test_param = 'hjgjgjhg'
         result = testing.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
 
-unittest.testing()
+    def test_do_stuff3(self):
+        test_param = None
+        result = testing.do_stuff(test_param)
+        self.assertIsInstance(result, 'please enter number')
 
+    def test_do_stuff4(self):
+        test_param = ''
+        result = testing.do_stuff(test_param)
+        self.assertIsInstance(result, 'please enter number')
+
+    def tearDown(self):
+        print('cleaning up')
+
+if __name__ == '__main__'
+    unittest.testing()
+
+# why is this useful? - you have more than one file and each module tested
+# ideally you want all of these tests run in unison
+# python3 -m unittest -v
+# -v = verbose (Which tests have we run and which are ok and which have failed)
+
+# setup will run before each function
--- testing/testing.py
+++ testing/testing.py
@@ -9,8 +9,13 @@
 # pep 8
 # - these allow us to check for simple styling and mistakes but only goes so far
 
-def do_stuff(num):
+# https://docs.python.org/3/library/unittest.html
+
+def do_stuff(num=0):
     try:
+        if num:
         return int(num) + 5
+    else:
+        return 'please enter number'
     except ValueError as err:
         return err
