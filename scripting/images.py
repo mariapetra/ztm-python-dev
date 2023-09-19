d613ea345e70576dc3ff0c05c6fca8980fcb66c0
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-19 17:40:27 +0800

    play with editing images in python

--- scripting/images.py
+++ scripting/images.py
@@ -5,6 +5,14 @@ filtered_img = img.filter(ImageFilter.EMBOSS)
 
 filtered_img = img.convert("L")
 filtered_img.save("grey.png", "png")
+filtered_img.show()
+filtered_img.rotate(90)
+resize = filtered_img.resize((300, 300))
+resize.save("newsize.png", "png")
+
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save("crop.png", "png")
 
 print(img.format)
 print(img.size)
