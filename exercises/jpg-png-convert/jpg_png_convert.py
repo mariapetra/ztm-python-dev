07c77f1f105673e0fc0cdd8a7ea236e09e9b7dcb
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-23 19:19:28 +0800

    update jpg exercise

--- exercises/jpg-png-convert/jpg_png_convert.py
+++ exercises/jpg-png-convert/jpg_png_convert.py
@@ -11,4 +11,6 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 for filename in os.listdir(image_folder):
-    img = Image.open(f"{image_folder}{filename}.png,'png")
+    img = Image.open(f"{image_folder}{filename}")
+    clean_name = os.path.splitext(filename[0])
+    img.save(f"{output_folder}{clean_name}.png", "png")
