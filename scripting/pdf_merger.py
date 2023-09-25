b8fcc359731c73aeddd7a9138891c5580d13cf1b
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-25 14:16:48 +0800

    remove commented code

--- scripting/pdf_merger.py
+++ scripting/pdf_merger.py
@@ -13,13 +13,3 @@ def pdf_combiner(pdf_list):
 
 
 pdf_combiner(inputs)
-
-# with open("twopage.pdf", "rb") as file:
-#     reader = PyPDF2.PdfReader(file)
-#     # page_edit = len(reader.pages)
-#     page_edit = reader.pages[0]
-#     page_edit.rotate(90)
-#     writer = PyPDF2.PdfWriter()
-#     writer.add_page(page_edit)
-#     with open("tilt.pdf", "wb") as new_file:
-#         writer.write(new_file)
