afc876f6db48f8d434d09ebf6cb7901d3764b242
Maria Claydon <62734302+mariapetra@users.noreply.github.com>
2023-09-27 09:58:47 +0800

    customise emails with py and html

--- scripting/send_emails.py
+++ scripting/send_emails.py
@@ -3,38 +3,19 @@
 # # https://docs.python.org/3/library/email.examples.html
 # # https://docs.python.org/3/library/email.html#module-email
 # # https://www.geeksforgeeks.org/simple-mail-transfer-protocol-smtp/
-
-# import smtplib
-# from email.message import EmailMessage
-
-# # to send emails you need a server - this is done through smtp
-# # standard for emails to be sent from computer to computer
-# email = EmailMessage()
-# email["from"] = "Maria Claydon"
-# email["to"] = "claydonmaria@gmail.com"
-# email["subject"] = " You won 1,000,000 dollars!"
-
-# email.set_content("I am a Python Master")
-
-# with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
-#     # serverPW = "qszplvznnetuhgcl"
-#     smtp.ehlo()
-#     # hello this is a server I am awake message
-#     smtp.starttls()
-#     # tls = enxryption mechanism - connect securely to the server
-#     smtp.login("mariatestingpython@gmail.com", "csvr nnud gpsb gmuh")
-#     # connect to your email account
-#     smtp.send_message(email)
-#     print("all good boss")
+# https://treyhunner.com/2018/12/why-you-should-be-using-pathlib/
 
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email["from"] = "Maria Claydon"
 email["to"] = "mariatestingpython@gmail.com"
 email["subject"] = "You won 1,000,000 dollars!"
-email.set_content("I am unstoppable")
+email.set_content(html.substitute({name: 'tintin'}), 'html')
 
 with smtplib.SMTP(host="smtp.gmail.com", port=486) as smtp:
     smtp.ehlo()
