63f857ad04409f8a5953e44e3fe5f8ddd9797eab
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-09-27 20:18:41 +0800

    finish password checker project

--- projects/password_checker.py/check_my_pass.py
+++ projects/password_checker.py/check_my_pass.py
@@ -1,8 +1,6 @@
 import requests
-
-url = "https://api.pwnedpasswords.com/range/" + "CBFDA"
-res = requests.get(url)
-print(res)
+import hashlib
+import sys
 
 # uses something called hashing
 # you should never store a users password in plain text
@@ -14,3 +12,44 @@ print(res)
 # https://pypi.org/project/requests/
 # https://passwordsgenerator.net/sha1-hash-generator/
 # https://haveibeenpwned.com/Passwords
+
+
+def request_api_data(query_char):
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f"Error fetching: {res.status_code}, check api and try again"
+        )
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f"{password} was found {count} times.. you should probably change your password"
+            )
+        else:
+            print(f"{password} not found carry on!")
+        return "done"
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
