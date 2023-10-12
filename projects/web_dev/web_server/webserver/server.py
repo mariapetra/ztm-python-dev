e6fe70cb72c3261bda25278c770d020d9f268f8b
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-10-12 20:57:41 +0800

    fix incorrect static route

--- projects/web_dev/web_server/webserver/server.py
+++ projects/web_dev/web_server/webserver/server.py
@@ -2,16 +2,10 @@ from flask import Flask, render_template, send_from_directory, url_for
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def my_home():
     return render_template("index.html")
 
-
-@app.route("/<string:pag_name>")
+@app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
-
-# @app.route("/submit_form" methods=['POST', 'GET'])
-# def submit_form():
-#     retr
